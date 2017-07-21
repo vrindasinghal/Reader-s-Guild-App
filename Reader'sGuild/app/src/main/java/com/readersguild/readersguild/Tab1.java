73926a45decb982cc9d6d7fa17633a4e6bd2e18a
vrindasinghal <vrinda74835@gmail.com>
2017-07-21 20:44:03 +0530

    21 July'17

--- Reader'sGuild/app/src/main/java/com/readersguild/readersguild/Tab1.java
+++ Reader'sGuild/app/src/main/java/com/readersguild/readersguild/Tab1.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,8 +18,33 @@ public class Tab1 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //Returning the layout file after inflating
-        //Change R.layout.tab1 in you classes
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.tab_1, container, false);
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+        return;
+    }
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        Intent i;
+        switch (item.getItemId()) {
+            case R.id.action_param:
+                i = new Intent(this, Settings.class);
+                startActivity(i);
+                return true;
+
+            case R.id.action_history:
+                i = new Intent(this, HistoryMenu.class);
+                startActivity(i);
+                return true;
+        }
+        return onOptionsItemSelected(item);
+    }*/
 }
