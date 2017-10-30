213afcf3ba7f739992fca1a70de0a101655b6357
vrindasinghal <vrinda74835@gmail.com>
2017-10-30 19:00:39 +0530

    Search_Contributor_Link_Added
    A custom dialog for searching contributor added.

---
+++ Reader'sGuild/app/src/main/java/com/readersguild/readersguild/ContributorsListActivity.java
@@ -0,0 +1,104 @@
+package com.readersguild.readersguild;
+
+import android.app.Dialog;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vrinda on 30-10-2017.
+ */
+
+public class ContributorsListActivity extends AppCompatActivity {
+
+    DataHelpAddBook dataHelpAddBook;
+    MyOpenHelper myOpenHelper;
+
+    private ArrayList<String> arrayListBookName;
+    private ArrayList<Integer> arrayListBookId;
+    private ArrayList<String> arrayListContributorsName;
+    private ArrayList<Integer> arrayListContributorsRollNumber;
+
+    private TextView searchLink;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contributors_list);
+        ListView listView_custom_contributors_list = (ListView) findViewById(R.id.listView_custom_contributors_list);
+
+        myOpenHelper = new MyOpenHelper(this);
+        dataHelpAddBook = new DataHelpAddBook(this);
+
+        arrayListBookName = new ArrayList<String>();
+        arrayListBookId = new ArrayList<Integer>();
+        arrayListContributorsName = new ArrayList<String>();
+        arrayListContributorsRollNumber = new ArrayList<Integer>();
+
+        getContributorsName();
+
+        CustomAdapterSecond adapter = new CustomAdapterSecond(ContributorsListActivity.this, R.layout.activity_custom_list_view_second, arrayListBookName, arrayListBookId, arrayListContributorsName, arrayListContributorsRollNumber);
+        listView_custom_contributors_list.setAdapter(adapter);
+
+        searchLink= (TextView) findViewById(R.id.search_button);
+
+        searchLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog(){
+        final Dialog dialog = new Dialog(ContributorsListActivity.this);
+        dialog.setContentView(R.layout.activity_custom_dialog_search_contributor);
+        dialog.setTitle("Search Contributor");
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonFindContributor);
+        //if button is clicked, close the custom dialog
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private void getContributorsName() {
+        Cursor c1;
+        try {
+            /*OPEN OR CREATE DATABASE*/
+            dataHelpAddBook.db = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME, 0, null);
+            /*THIS METHOD IS USED TO GET ALL RECORDS FROM DATABASE*/
+            c1 = dataHelpAddBook.getBookRecords();
+            if (c1.moveToFirst()) {
+                Log.v("Message in C1", "Not Null");
+                do {
+                    Integer BookId = c1.getInt(0);
+                    String bookName = c1.getString(1);
+                    String contributorsName = c1.getString(2);
+                    Integer contributorsRollNumber = c1.getInt(3);
+                    arrayListBookId.add(BookId);
+                    arrayListBookName.add(bookName);
+                    arrayListContributorsName.add(contributorsName);
+                    arrayListContributorsRollNumber.add(contributorsRollNumber);
+                    Log.v("srNumber", "" + BookId);
+                    Log.v("slipDate", "" + bookName);
+                } while (c1.moveToNext());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+}
